bc77ea31eda7ac6ebac3cd9f73d2508548379ec2
Prakash842277 <121437397+Prakash842277@users.noreply.github.com>
2023-04-12 14:43:05 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,27 @@
-import React from 'react'
-import '../styles/App.css';
-import { Wrapper } from '../context/userContext';
-import { Settings } from './Settings';
-import { Home } from './Home';
+import React from "react";
+import "../styles/App.css";
+import Component from "./Component";
+import ErrorBoundary from "./ErrorBoundary";
 const App = () => {
+  let throwErrorFunction;
+  let errorGenerator = () => {
+    throwErrorFunction(true);
+  };
 
+  let changeFunc = (updateError) => {
+    throwErrorFunction = updateError;
+  };
   return (
     <div id="main">
-      <Wrapper>
-        <Settings />
-        <Home />
-      </Wrapper>
+      <h2>React Error Boundaries</h2>
+      <ErrorBoundary>
+        <Component change={changeFunc} />
+      </ErrorBoundary>
+      <button id="gen" onClick={errorGenerator}>
+        Generate Error
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
